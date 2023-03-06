e6fadd3087812919711c3ec9b3f4217f10e9420c
Leandro Freire <fotografoleandro81@gmail.com>
2023-03-06 16:16:03 -0300

    carga de archivos

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Home = () => {
+
+  const [ files, setFiles ] = useState([]);
+
+  function handleOnInput(e){
+    setFiles([...files, e.target.files])
+    console.log('archivos: ', files)
+  }
+
+  async function handleOnButton(e){
+    console.log('archivos: ', files)
+    const f = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      f.append("files", files[i]);
+    }
+
+    console.log('mostrar: ', f)
+  }
+
   return (
     <div>
-        <input type='file' name='archivosSubidos' accept='.jpg' multiple/>
+        <br/>
+        <input type='file' name='files' accept='.jpg' multiple onChange={handleOnInput} />
+        <br/><br/>
+        <button onClick={handleOnButton}>Agregar</button>
     </div>
  
     )
