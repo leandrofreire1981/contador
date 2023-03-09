5f57c00d618a7778a46726893629022a7a71eb7b
Leandro Freire <fotografoleandro81@gmail.com>
2023-03-09 19:33:52 -0300

    9-3

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,28 +2,37 @@ import React, { useState } from 'react'
 
 export const Home = () => {
 
-  const [ files, setFiles ] = useState([]);
+  const [ files, setFiles ] = useState('');
 
   function handleOnInput(e){
-    setFiles([...files, e.target.files])
+    setFiles(e.target.files[0])
     console.log('archivos: ', files)
   }
 
   async function handleOnButton(e){
     console.log('archivos: ', files)
-    const f = new FormData();
+    const f = new FileReader()
+    f.readAsText(files)
 
-    for (let i = 0; i < files.length; i++) {
-      f.append("files", files[i]);
+    f.onload = () => {
+     // for (let i = 0; i < 7; i++) {
+        
+     console.log(f.result.includes('Build'))
+
+      //}
     }
+    f.onerror = () => {
+      console.log('error: ', f.error)
+    }
+ 
 
-    console.log('mostrar: ', f)
+ 
   }
 
   return (
     <div>
         <br/>
-        <input type='file' name='files' accept='.jpg' multiple onChange={handleOnInput} />
+        <input type='file' name='files' accept='.gcode' multiple onChange={handleOnInput} />
         <br/><br/>
         <button onClick={handleOnButton}>Agregar</button>
     </div>
