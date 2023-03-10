5d10b20cbe7044a5d904589c0687f3ca6b492f5c
Leandro Freire <fotografoleandro81@gmail.com>
2023-03-10 17:44:49 -0300

    10-03

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,31 +2,49 @@ import React, { useState } from 'react'
 
 export const Home = () => {
 
-  const [ files, setFiles ] = useState('');
+  const [ files, setFiles ] = useState([]);
+  const [ results, setResults ] = useState([])
 
   function handleOnInput(e){
-    setFiles(e.target.files[0])
+    if(files.length === 0)
+      setFiles([...e.target.files])
+    else
+      setFiles([...files, ...e.target.files])
     console.log('archivos: ', files)
   }
 
   async function handleOnButton(e){
     console.log('archivos: ', files)
-    const f = new FileReader()
-    f.readAsText(files)
+    //const f = new FileReader()
+    //f.readAsText(files[0])
 
-    f.onload = () => {
-     // for (let i = 0; i < 7; i++) {
-        
-     console.log(f.result.includes('Build'))
+    const f = []
+    const auxF = []
+    for (let i = 0; i < files.length; i++) {
+      f[i] = new FileReader()
+      f[i].readAsText(files[i])
 
-      //}
-    }
-    f.onerror = () => {
-      console.log('error: ', f.error)
-    }
- 
+      f[i].onload = () => {
+        for (let j = 0; j < f[i].result.length; j++) {
+          let aux = f[i].result.slice(j, j + 5)
+          if(aux === 'Build'){
+            auxF.push(f[i].result.slice(j))
+            if(results.length === 0)
+              setResults([f[i].result.slice(j)])
+            else  
+              setResults([results, f[i].result.slice(j)])
+            i++
+            break
+          }
+        }
+        console.log('mostrar: ', auxF)
+      }
 
- 
+      f[i].onerror = () => {
+        console.log('error: ', f.error)
+      }
+    } 
+    
   }
 
   return (
@@ -35,6 +53,16 @@ export const Home = () => {
         <input type='file' name='files' accept='.gcode' multiple onChange={handleOnInput} />
         <br/><br/>
         <button onClick={handleOnButton}>Agregar</button>
+        <br></br>
+        <br></br>
+        <div>
+          Mostrando resultados
+        </div>
+        <div>
+          {results.length>0 && results.map((r, i) => (
+            <div key={'code' + i}>{r} </div>
+          ))}
+        </div>
     </div>
  
     )
